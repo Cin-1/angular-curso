1e9eadd1dece27710e1035959c2508b2521e4801
cinthia <cinthiapardos@gmail.com>
2023-05-02 18:41:13 -0300

    register component

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './page/login-page.component';
+import { RegisterFormComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent,
   },
 ];
 
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './page/login-page.component';
 import { SharedModule } from '@shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterFormComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterFormComponent],
   imports: [CommonModule, AuthRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class AuthModule {}
