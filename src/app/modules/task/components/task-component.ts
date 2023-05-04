c09c14123a7d766d1ffdfd979bcb23ccfbb058e7
cinthia <cinthiapardos@gmail.com>
2023-05-04 10:17:01 -0300

    directivas

--- src/app/modules/task/components/task-component.ts
+++ src/app/modules/task/components/task-component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-component',
   templateUrl: './task-component.html',
   styleUrls: ['./task-component.css'],
 })
-export class TaskComponent {}
+export class TaskComponent implements OnInit {
+  mostrarElemento: boolean = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  mostrarElementof(): void {
+    !this.mostrarElemento;
+  }
+}
